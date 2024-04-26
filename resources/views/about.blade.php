7c9a9615c59ac5be10bfd12fed5d338af48bb003
adimiuprix <miuprix@gmail.com>
2024-04-26 20:11:02 +0700

    Install tailwind, configs , layouting

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -141,7 +141,7 @@
         <main>
             <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
                 <!-- Your content -->
-                <p>Contact me!!</p>
+                <p>About me</p>
             </div>
         </main>
     </div>
