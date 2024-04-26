f54214adf3511a1a82bcef3263b55e4ad70231ec
adimiuprix <miuprix@gmail.com>
2024-04-26 19:30:06 +0700

    phase 1

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog</title>
+</head>
+    <body>
+        <a href="/">Home</a>
+        <a href="/blog">Blog</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+
+        <h1>Halaman blog</h1>
+
+        <article>
+            <h3>Judul Artikel 1</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, accusantium obcaecati. Tempore, dolor odio quas similique porro recusandae quia aliquam reiciendis eaque autem exercitationem quod error facere modi cumque laudantium.</p>
+        </article>
+
+        <article>
+            <h3>Judul Artikel 1</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, accusantium obcaecati. Tempore, dolor odio quas similique porro recusandae quia aliquam reiciendis eaque autem exercitationem quod error facere modi cumque laudantium.</p>
+        </article>
+
+        <article>
+            <h3>Judul Artikel 1</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, accusantium obcaecati. Tempore, dolor odio quas similique porro recusandae quia aliquam reiciendis eaque autem exercitationem quod error facere modi cumque laudantium.</p>
+        </article>
+
+    </body>
+</html>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Kontak</title>
+</head>
+    <body>
+        <a href="/">Home</a>
+        <a href="/blog">Blog</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+
+        <h1>Halaman kontak</h1>
+
+        <ul>
+            <li><a href="http://">Facebook</a></li>
+            <li><a href="http://">Twitter</a></li>
+            <li><a href="http://">Youtube</a></li>
+        </ul>
+    </body>
+</html>
