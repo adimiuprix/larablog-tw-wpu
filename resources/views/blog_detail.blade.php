da3ea3b400990cd311a033a21932fbf03dbc27da
adimiuprix <miuprix@gmail.com>
2024-06-21 14:35:50 +0700

    UI design phase 2

--- resources/views/blog_detail.blade.php
+++ resources/views/blog_detail.blade.php
@@ -22,22 +22,23 @@
         <div class="flex justify-between px-4 mx-auto max-w-screen-xl ">
             <article class="mx-auto w-full max-w-4xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
                 <header class="mb-4 lg:mb-6 not-format">
+                    <a href="/blog" class="font-medium text-sm text-blue-600 hover:underline">&laquo; Back to all post</a>
                     <address class="flex items-center mb-6 not-italic">
                         <div class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
                             <img class="mr-4 w-16 h-16 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-2.jpg" alt="Jese Leos">
                             <div>
-                                <a href="#" rel="author" class="text-xl font-bold text-gray-900 dark:text-white">Jese Leos</a>
-                                <p class="text-base text-gray-500 dark:text-gray-400">Graphic Designer, educator & CEO Flowbite</p>
-                                <p class="text-base text-gray-500 dark:text-gray-400"><time pubdate datetime="2022-02-08" title="February 8th, 2022">{{ $post->created_at->diffForHumans() }}</time></p>
+                                <a href="authors/{{ $post->author->username }}" rel="author" class="text-xl font-bold text-gray-900 dark:text-white" alt="{{ $post->author->name }}">{{ $post->author->name }}</a>
+                                <p class="text-base text-gray-500 dark:text-gray-400">{{ $post->created_at->diffForHumans() }}</p>
+                                <span class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
+                                    <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+                                </span>
                             </div>
                         </div>
                     </address>
                     <h1 class="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">{{ $post->title }}</h1>
                 </header>
 
-                <p>Before going digital, you might benefit from scribbling down some ideas in a sketchbook. This way,
-                    you can think things through before committing to an actual design project.
-                </p>
+                <p>{{ $post->body }}</p>
             </article>
         </div>
     </main>
